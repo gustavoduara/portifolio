2fef91f66bf3a64d98b129d65b626e5c766c6756
Gustavo Duara <gugaduara@gmail.com>
2025-10-03 10:35:06 -0300

    adiconado index.js e concertado bug logo github

---
+++ index.js
@@ -0,0 +1,90 @@
+const dadosCardDuoBingo = {
+    titulo: 'DuoBingo',
+    imagem: './assets/controle-de-medicamentos.png',
+    gif: 'https://i.imgur.com/SDgwx8m.gif',
+    urlRepositorio: 'https://github.com/academiadoprogramador-fullstack/controle-de-medicamentos-2025',
+    stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+    ],
+    descricao: `O sistema de Controle de Medicamentos apresenta o conceito de gerenciamento de um posto de saúde, com o controle de fornecimento, estoque, funcionários, pacientes e retirada de prescrições — com foco na rastreabilidade das movimentações de estoque.`
+};
+
+const dadosCardGestaoEquipamentos = {
+    titulo: 'Gestão de Equipamentos',
+    imagem: './assets/gestao-de-equipamentos.png',
+    gif: '',
+    urlRepositorio: 'https://github.com/academiadoprogramador-fullstack/gestao-de-equipamentos-2025',
+    stack: [
+        'https://skillicons.dev/icons?i=html&theme=light',
+        'https://skillicons.dev/icons?i=css&theme=light',
+        'https://skillicons.dev/icons?i=cs&theme=light',
+        'https://skillicons.dev/icons?i=net&theme=light',
+    ],
+    descricao: `O sistema de Gestão de Equipamentos controla o estoque de um almoxarifado, com a gestão de equipamentos e o registro de chamados de manutenção que eventualmente ocorrem durante o uso dos equipamentos. Também são inclusas informações sobre os fabricantes dos equipamentos para contato de suporte ou compras.`
+}
+
+let cardSelecionado;
+
+function carregarDadosCardAtual() {
+    if (!cardSelecionado)
+        throw new Error('Card não selecionado.');
+
+    const imgCard = document.getElementById('imgCard');
+    const stackCard = document.getElementById('stackCard');
+    const tituloCard = document.getElementById('tituloCard');
+    const descricaoCard = document.getElementById('descricaoCard');
+    const urlRepositorio = document.getElementById('urlRepositorio');
+
+    imgCard.src = cardSelecionado.imagem;
+    tituloCard.textContent = cardSelecionado.titulo;
+    descricaoCard.textContent = cardSelecionado.descricao;
+    urlRepositorio.href = cardSelecionado.urlRepositorio;
+
+    stackCard.innerHTML = '';
+
+    for (let item of cardSelecionado.stack) {
+        const iconeStack = document.createElement('img');
+        iconeStack.width = 35;
+        iconeStack.src = item;
+
+        stackCard.appendChild(iconeStack);
+    }
+}
+
+function selecionarPrimeiroCard() {
+    if (cardSelecionado == dadosCardControleMedicamentos)
+        return;
+
+    cardSelecionado = dadosCardControleMedicamentos;
+    carregarDadosCardAtual();
+}
+
+function selecionarSegundoCard() {
+    if (cardSelecionado == dadosCardGestaoEquipamentos)
+        return;
+
+    cardSelecionado = dadosCardGestaoEquipamentos;
+    carregarDadosCardAtual();
+}
+
+function abrirModalProjeto() {
+    document.getElementById('portfolioModalLabel').textContent = cardSelecionado.titulo;
+    document.getElementById('portfolioModalImage').src = cardSelecionado.gif;
+}
+
+function main() {
+    const btnSelecionarPrimeiroCard = document.getElementById("btnSelecionarPrimeiroCard");
+    const btnSelecionarSegundoCard = document.getElementById("btnSelecionarSegundoCard");
+    const btnVerProjeto = document.getElementById("btnVerProjeto")
+
+    btnSelecionarPrimeiroCard.addEventListener('click', selecionarPrimeiroCard);
+    btnSelecionarSegundoCard.addEventListener('click', selecionarSegundoCard);
+    btnVerProjeto.addEventListener('click', abrirModalProjeto);
+
+    selecionarPrimeiroCard();
+};
+
+document.addEventListener('DOMContentLoaded', main);
